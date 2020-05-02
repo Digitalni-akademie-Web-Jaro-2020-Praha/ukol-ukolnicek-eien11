1297b1fdb6cb683242b77de2b4cccb2907c46896
Adela <adela.matouskova1@gmail.com>
2020-05-02 17:37:29 +0200

    Developed without git

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('funguju!');
+let tasks = [];
+
+const updateTasks = () => {
+  let tasksElm = document.querySelector('.todo__tasks');
+  tasksElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    tasksElm.innerHTML += `<div class="task">${tasks[i]}</div>`
+  }
+}
+
+const addTask = () => {
+  const inputElm = document.querySelector('#new-task');
+  if (inputElm.value) {
+    tasks.push(inputElm.value);
+    updateTasks();
+    inputElm.value = '';
+  }
+}
+
+document.querySelector('.btn-add').addEventListener('click', addTask);
+
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    addTask();
+  }
+})
